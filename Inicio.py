2863c5cddd888007e9b5360d43e6bd1b0800cc45
C4-T0 <carlostorresh1@gmail.com>
2017-03-27 23:29:29 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -322,7 +322,7 @@ def ComentarObra(artista):
 	opciones = Articulo.BuscarArticulo(nombre)
 	print(Texto.mensajesComentarios["encontrados"])
 	for clave in opciones:
-		print(Texto.mensajesComentarios["nombre"],opciones[clave].getNombre(),Texto.mensajesComentarios["idobra"],clave)
+		print(Texto.mensajesObras["nombre"],opciones[clave].getNombre(),Texto.mensajesObras["id"],clave)
 	eleccion = str(input(Texto.mensajesComentarios["ingresarid"]))
 	MenuAgregarComentario(artista, opciones[eleccion])
 
