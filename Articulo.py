31c0731314fd1ebae0601978c85ed852600c6f10
saarboledaz <sergioarboledazuluaga@gmail.com>
2017-03-29 17:07:31 -0500

    Update Articulo.py

--- Articulo.py
+++ Articulo.py
@@ -6,52 +6,52 @@ from Textos import Texto
 
 
 class Articulo():
-    dictArticulos = {}
-    iden = 1
+	dictArticulos = {}
+	iden = 1
 
-    def __init__(self, nombre, precio, artista, tipo, descripcion, fromRegister=False):
-        self._precio = None
-        self._nombre = None
-        self._artista = None
-        self._descripcion = None
-        self._puntuacion = None
-        self._comentarios = dict()
-        self._fechaPublicacion = date.today()
-        self._tipo = None
-        self._estado = True
-        self._id = str(Articulo.iden)
-        self.setNombre(nombre)
-        self.setPrecio(precio, fromRegister)
-        self.setArtista(artista)
-        self.setTipo(tipo)
-        self.setDescripcion(descripcion)
-        artista.addArticulo(self)
-        Articulo.dictArticulos[str(Articulo.iden)] = self
-        Articulo.iden += 1
+	def __init__(self, nombre, precio, artista, tipo, descripcion, fromRegister=False):
+		self._precio = None
+		self._nombre = None
+		self._artista = None
+		self._descripcion = None
+		self._puntuacion = None
+		self._comentarios = dict()
+		self._fechaPublicacion = date.today()
+		self._tipo = None
+		self._estado = True
+		self._id = str(Articulo.iden)
+		self.setNombre(nombre)
+		self.setPrecio(precio, fromRegister)
+		self.setArtista(artista)
+		self.setTipo(tipo)
+		self.setDescripcion(descripcion)
+		artista.addArticulo(self)
+		Articulo.dictArticulos[str(Articulo.iden)] = self
+		Articulo.iden += 1
 
-    # ------------------------------------------SETTERS------------------------------------------------------------------
+	# ------------------------------------------SETTERS------------------------------------------------------------------
 
-    def setPrecio(self, precio, fromRegister):
-        aux = precio
-        while (True):
-            if (precio >= 0):
-                self._precio = precio
-                break
-            else:
-                print(Texto.mensajesObras["aprecio"])
-                aux = int(input(Texto.mensajesObras["precio"]))
+	def setPrecio(self, precio, fromRegister):
+		aux = precio
+		while (True):
+			if (precio >= 0):
+				self._precio = precio
+				break
+			else:
+				print(Texto.mensajesObras["aprecio"])
+				aux = int(input(Texto.mensajesObras["precio"]))
 
-    def setNombre(self, nombre):
-        self._nombre = nombre
+	def setNombre(self, nombre):
+		self._nombre = nombre
 
-    def setArtista(self, artista):
-        self._artista = artista
+	def setArtista(self, artista):
+		self._artista = artista
 
-    def setDescripcion(self, descripcion):
-        self._descripcion = descripcion
+	def setDescripcion(self, descripcion):
+		self._descripcion = descripcion
 
-    def addComentario(self, comentario):
-        self._comentarios[comentario._id] = comentario
+	def addComentario(self, comentario):
+		self._comentarios[comentario._id] = comentario
 
 	def setPuntuacion(self):
 		cont = 0
@@ -62,115 +62,132 @@ class Articulo():
 		else:
 			self._puntuacion = None
 
-    def setFechaPublicacion(self):
-        self._fechaPublicacion = date.today()
 
-    def setTipo(self, tipo):
-        self._tipo = tipo
+	def setFechaPublicacion(self):
+		self._fechaPublicacion = date.today()
 
-    def setEstado(self, estado):
-        self._estado = estado
+	def setTipo(self, tipo):
+		self._tipo = tipo
 
-    # -------------------------------------GETTERS-----------------------------------------------------------------------
+	def setEstado(self, estado):
+		self._estado = estado
 
-    def getPrecio(self):
-        return self._precio
+	# -------------------------------------GETTERS-----------------------------------------------------------------------
 
-    def getNombre(self):
-        return self._nombre
+	def getPrecio(self):
+		return self._precio
 
-    def getArtista(self):
-        return self._artista
+	def getNombre(self):
+		return self._nombre
 
-    def getDescripcion(self):
-        return self._descripcion
+	def getArtista(self):
+		return self._artista
 
-    def getComentarios(self):
-        return self._comentarios
+	def getDescripcion(self):
+		return self._descripcion
 
-    def getPuntuacion(self):
-        return self._puntuacion
+	def getComentarios(self):
+		return self._comentarios
 
-    def getFechaPublicacion(self):
-        return self._fechaPublicacion
+	def getPuntuacion(self):
+		return self._puntuacion
 
-    def getId(self):
-        return int(self._id)
+	def getFechaPublicacion(self):
+		return self._fechaPublicacion
 
-    def getTipo(self):
-        return self._tipo
+	def getId(self):
+		return int(self._id)
 
-    def getEstado(self):
-        return self._estado
+	def getTipo(self):
+		return self._tipo
 
-    # --------------------------------------METODOS ESTATICOS---------------------------------
-    @staticmethod
-    def AgregarArticulo(nombre, precio, artista, tipo, descripcion):
-        nuevoArticulo = Articulo(nombre, precio, artista, tipo, descripcion, True)
-        return nuevoArticulo
+	def getEstado(self):
+		return self._estado
 
-    @staticmethod
-    def BuscarArticulo(nombre):
-        related = dict()
-        palabras = nombre.split(' ')
-        for palabra in palabras:
-            for i in Articulo.dictArticulos:
-                if ((Articulo.dictArticulos[i].getNombre().find(palabra)) >= 0 and
-                        not (i in related.keys())):
-                    related[i] = Articulo.dictArticulos[i]
-        return related
+	# --------------------------------------METODOS ESTATICOS---------------------------------
+	@staticmethod
+	def AgregarArticulo(nombre, precio, artista, tipo, descripcion):
+		nuevoArticulo = Articulo(nombre, precio, artista, tipo, descripcion, True)
+		return nuevoArticulo
 
-    # -----------------------------------FUNCIONALIDADES----------------------------------------
-    def EditarArticulo(self, artista, nombre, precio, tipo, descripcion):
-        if (artista.getId() == self.getArtista().getId()):
-            self.setNombre(nombre)
-            self.setPrecio(precio, True)
-            self.setTipo(tipo)
-            self.setDescripcion(descripcion)
-            return True
-        else:
-            return False
+	@staticmethod
+	def BuscarArticulo(nombre):
+		related = dict()
+		palabras = nombre.split(' ')
+		for palabra in palabras:
+			for i in Articulo.dictArticulos:
+				if ((Articulo.dictArticulos[i].getNombre().find(palabra)) >= 0 and
+						not (i in related.keys())):
+					related[i] = Articulo.dictArticulos[i]
+		return related
 
-    def EliminarArticulo(self, artista):
-        if (artista.getId() == self.getArtista().getId()):
-            artista = self.getArtista()
-            iden = self._id
-            del artista._articulos[iden]
-            del Articulo.dictArticulos[iden]
-            return True
-        else:
-            return False
+	# -----------------------------------FUNCIONALIDADES----------------------------------------
+	def EditarArticulo(self, artista, nombre, precio, tipo, descripcion):
+		if (artista.getId() == self.getArtista().getId()):
+			self.setNombre(nombre)
+			self.setPrecio(precio, True)
+			self.setTipo(tipo)
+			self.setDescripcion(descripcion)
+			return True
+		else:
+			return False
 
-    def info(self):
-        return {self._nombre, self._precio, self._tipo, self._descripcion, self._puntuacion, self._id}
+	def EliminarArticulo(self, artista):
+		if (artista.getId() == self.getArtista().getId()):
+			artista = self.getArtista()
+			iden = self._id
+			del artista._articulos[iden]
+			del Articulo.dictArticulos[iden]
+			return True
+		else:
+			return False
 
-    def ObtenerNumeroVistas(self):
-        pass
+	def info(self):
+		return {self._nombre, self._precio, self._tipo, self._descripcion, self._puntuacion, self._id}
 
-    def MostrarMejorArticulo(self):
-        mejoresArticulos = []
-        for x in range(5, 0):
-            for i in Articulo.dictArticulos:
-                if (Articulo.dictArticulos[i].getPuntuacion == x):
-                    mejoresArticulos.append(i)
-            if (len(mejoresArticulos) > 0):
-                break
-        return mejoresArticulos
 
-    def ComprarArticulo(self, usuario):
-        if (usuario.getPresupuesto() >= self.getPrecio()):
-            artista = self.getArtista()
-            artista._articulosVendidos[str(self.getId())] = self
-            usuario._obrasCompradas[str(self.getId())] = self
-            usuario.RestarPresupuesto(self.getPrecio())
-            self.setEstado(False)
-            del artista._articulos[str(self.getId())]
-            del Articulo.dictArticulos[str(self.getId())]
-            return True
-        else:
-            return False
 
-    @staticmethod
-    def GenerarDatosFicticios():
-        Articulo("Las medias 1", 900, Artista.dictArtistasID["4"], "fisico", "gay")
-        Articulo("Las camisas 2", 901, Artista.dictArtistasID["4"], "digital", "gay")
+	def ObtenerNumeroVistas(self):
+		pass
+
+	def MostrarMejorArticulo(self):
+		mejoresArticulos = []
+		for x in range(5, 0):
+			for i in Articulo.dictArticulos:
+				if (Articulo.dictArticulos[i].getPuntuacion == x):
+					mejoresArticulos.append(i)
+			if (len(mejoresArticulos) > 0):
+				break
+		return mejoresArticulos
+
+	def ComprarArticulo(self, usuario):
+		if (usuario.getPresupuesto() >= self.getPrecio()):
+			artista = self.getArtista()
+			artista._articulosVendidos[str(self.getId())] = self
+			usuario._obrasCompradas[str(self.getId())] = self
+			usuario.RestarPresupuesto(self.getPrecio())
+			self.setEstado(False)
+			del artista._articulos[str(self.getId())]
+			del Articulo.dictArticulos[str(self.getId())]
+			return True
+		else:
+			return False
+
+	@staticmethod
+	def OrdenarPorReputacion():
+		articulos = []
+		for i in Articulo.dictArticulos:
+			Articulo.dictArticulos[i].setPuntuacion()
+			if (Articulo.dictArticulos[i].getPuntuacion() != None):
+				articulos.append(Articulo.dictArticulos[i])
+		articulos.sort(key=lambda articulo: articulo._puntuacion, reverse=True)
+		return articulos
+
+
+
+
+
+	@staticmethod
+	def GenerarDatosFicticios():
+		Articulo("Las medias 1", 900, Artista.dictArtistasID["4"], "fisico", "gay")
+		Articulo("Las camisas 2", 901, Artista.dictArtistasID["4"], "digital", "gay")
